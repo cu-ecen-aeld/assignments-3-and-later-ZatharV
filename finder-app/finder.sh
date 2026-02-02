311c684d6012984d1fd159493b6181582d1fcc07
ZatharV <atharvmore2622@gmail.com>
2026-02-01 21:38:08 -0700

    Changed tree command

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -2,7 +2,7 @@
 
 #AI-chat: https:copilot.microsoft.com/shares/Wg6SSa5FNT5BSmBJ5DJvw
 #Line 5 added based on chat
-sudo apt install tree
+#sudo apt install tree
 
 if [ $# -ne 2 ]
 then
@@ -20,8 +20,8 @@ then
 	exit 1
 fi
 
-X=$((($(tree "$filesdir" | wc -l)) - $(tree -d "$filesdir" | wc -l)))
-
+#reference: https://chatgpt.com/share/698029c2-f794-8001-a3ed-bf2d59118082
+X=$(find "$filesdir" -type f | wc -l)
 Y=$(grep -r "$searchstr" "$filesdir" | wc -l)
 
 echo "The number of files are $X and the number of matching lines are $Y"

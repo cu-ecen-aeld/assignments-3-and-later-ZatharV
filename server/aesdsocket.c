287989323a90d8bd890f553b871d08d24bab4857
ZatharV <atharvmore2622@gmail.com>
2026-02-15 02:15:43 -0700

    update

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -10,18 +10,22 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <errno.h>
+#include <sys/stat.h>
 
 #define PORT 9000
 #define DATAFILE "/var/tmp/aesdsocketdata"
 
 static volatile sig_atomic_t exit_requested = 0;
-int sockfd = -1;
-int clientfd = -1;
+int server_fd = -1;
+int client_fd = -1;
 
 void signal_handler(int sig)
 {
     syslog(LOG_INFO, "Caught signal, exiting");
     exit_requested = 1;
+
+    if (client_fd != -1) close(client_fd);
+    if (server_fd != -1) close(server_fd);
 }
 
 int main(int argc, char *argv[])
@@ -31,6 +35,12 @@ int main(int argc, char *argv[])
 
     openlog("aesdsocket", LOG_PID, LOG_USER);
 
+    // Ensure /var/tmp exists (Buildroot often lacks it)
+    if (mkdir("/var/tmp", 0777) == -1 && errno != EEXIST) {
+        syslog(LOG_ERR, "Failed to create /var/tmp");
+        return -1;
+    }
+
     // Install signal handlers
     struct sigaction sa;
     memset(&sa, 0, sizeof(sa));
@@ -38,46 +48,42 @@ int main(int argc, char *argv[])
     sigaction(SIGINT, &sa, NULL);
     sigaction(SIGTERM, &sa, NULL);
 
-    // 1. Create socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
+    // Create socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
         syslog(LOG_ERR, "socket() failed");
-        perror("socket");
         return -1;
     }
 
     int opt = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    // 2. Bind to port 9000
+    // Bind
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(PORT);
 
-    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         syslog(LOG_ERR, "bind() failed");
-        perror("bind");
-        close(sockfd);
+        close(server_fd);
         return -1;
     }
 
-    // 3. Listen
-    if (listen(sockfd, 10) < 0) {
+    // Listen
+    if (listen(server_fd, 10) < 0) {
         syslog(LOG_ERR, "listen() failed");
-        perror("listen");
-        close(sockfd);
+        close(server_fd);
         return -1;
     }
 
-    // 4. Accept loop
+    // Accept loop
     while (!exit_requested) {
 
-        clientfd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_len);
-        if (clientfd < 0) {
-            if (exit_requested) break;  // interrupted by signal
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
+        if (client_fd < 0) {
+            if (exit_requested) break;
             syslog(LOG_ERR, "accept() failed");
-            perror("accept");
             continue;
         }
 
@@ -85,14 +91,13 @@ int main(int argc, char *argv[])
         inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
         syslog(LOG_INFO, "Accepted connection from %s", client_ip);
 
-        // Receive buffer
+        // Receive packet until newline
         char recvbuf[1024];
-        size_t total_len = 0;
         char *packet = NULL;
+        size_t total_len = 0;
 
-        // 5. Receive until newline
         while (!exit_requested) {
-            ssize_t bytes = recv(clientfd, recvbuf, sizeof(recvbuf), 0);
+            ssize_t bytes = recv(client_fd, recvbuf, sizeof(recvbuf), 0);
             if (bytes <= 0) break;
 
             char *newbuf = realloc(packet, total_len + bytes);
@@ -110,7 +115,7 @@ int main(int argc, char *argv[])
             if (memchr(recvbuf, '\n', bytes)) break;
         }
 
-        // 6. Append packet to file
+        // Append packet to file
         if (packet) {
             int fd = open(DATAFILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
             if (fd < 0) {
@@ -122,26 +127,25 @@ int main(int argc, char *argv[])
             free(packet);
         }
 
-        // 7. Send full file back
+        // Send full file back
         int fd = open(DATAFILE, O_RDONLY);
         if (fd >= 0) {
             char filebuf[1024];
             ssize_t r;
             while ((r = read(fd, filebuf, sizeof(filebuf))) > 0) {
-                send(clientfd, filebuf, r, 0);
+                send(client_fd, filebuf, r, 0);
             }
             close(fd);
         }
 
-        // 8. Close connection
         syslog(LOG_INFO, "Closed connection from %s", client_ip);
-        close(clientfd);
-        clientfd = -1;
+        close(client_fd);
+        client_fd = -1;
     }
 
-    // Cleanup on exit
-    if (clientfd != -1) close(clientfd);
-    if (sockfd != -1) close(sockfd);
+    // Cleanup
+    if (client_fd != -1) close(client_fd);
+    if (server_fd != -1) close(server_fd);
 
     remove(DATAFILE);
 

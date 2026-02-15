da71b479f4cc2a714742217956bf4c82555bd4da
ZatharV <atharvmore2622@gmail.com>
2026-02-15 01:52:37 -0700

    aesdsocket update

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <syslog.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netdb.h>
-#include <syslog.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <signal.h>
 #include <fcntl.h>
-#include <arpa/inet.h>
 #include <errno.h>
 
 #define PORT "9000"
@@ -16,23 +16,16 @@
 #define BUF_SIZE 1024
 
 int server_fd = -1;
+int data_fd = -1;
 
-// Signal handler
-void signal_handler(int sig) {
-    if (sig == SIGINT || sig == SIGTERM) {
-        syslog(LOG_INFO, "Caught signal, exiting");
-        
-        // Close the server socket to break the accept loop
-        if (server_fd != -1) {
-            close(server_fd);
-            server_fd = -1;
-        }
-    }
-}
-
-void cleanup() {
+// Signal handler for graceful shutdown
+void handle_signal(int sig) {
+    syslog(LOG_INFO, "Caught signal, exiting");
+    if (server_fd != -1) close(server_fd);
+    if (data_fd != -1) close(data_fd);
     remove(DATA_FILE);
     closelog();
+    exit(0);
 }
 
 int main(int argc, char *argv[]) {
@@ -45,103 +38,81 @@ int main(int argc, char *argv[]) {
 
     // Setup signal handling
     struct sigaction sa;
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = signal_handler;
+    sa.sa_handler = handle_signal;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
     sigaction(SIGINT, &sa, NULL);
     sigaction(SIGTERM, &sa, NULL);
 
-    // Socket address info setup
-    struct addrinfo hints, *res;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    if (getaddrinfo(NULL, PORT, &hints, &res) != 0) {
-        perror("getaddrinfo");
-        return -1;
-    }
-
-    server_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (server_fd == -1) {
-        freeaddrinfo(res);
-        return -1;
-    }
-
+    // Create Socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
     int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
-        freeaddrinfo(res);
-        close(server_fd);
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(9000);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("Bind failed");
         return -1;
     }
 
-    if (bind(server_fd, res->ai_addr, res->ai_addrlen) != 0) {
-        freeaddrinfo(res);
-        close(server_fd);
-        return -1;
-    }
-    freeaddrinfo(res);
-
-    // Daemonization block
+    // Daemonize if requested
     if (daemon_mode) {
-        pid_t pid = fork();
-        if (pid < 0) return -1;
-        if (pid > 0) exit(0); // Parent exits successfully
-        
+        if (fork() > 0) exit(0);
         setsid();
         chdir("/");
-        int dev_null = open("/dev/null", O_RDWR);
-        dup2(dev_null, STDIN_FILENO);
-        dup2(dev_null, STDOUT_FILENO);
-        dup2(dev_null, STDERR_FILENO);
-        close(dev_null);
+        int devnull = open("/dev/null", O_RDWR);
+        dup2(devnull, STDIN_FILENO);
+        dup2(devnull, STDOUT_FILENO);
+        dup2(devnull, STDERR_FILENO);
+        close(devnull);
     }
 
-    if (listen(server_fd, 10) != 0) {
-        close(server_fd);
-        return -1;
-    }
+    listen(server_fd, 10);
 
-    while (server_fd != -1) {
+    while (1) {
         struct sockaddr_in client_addr;
-        socklen_t addr_size = sizeof(client_addr);
-        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_size);
+        socklen_t client_len = sizeof(client_addr);
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
         
-        if (server_fd == -1) break; // Check if signal handler closed socket
-        if (client_fd == -1) continue;
-
         char client_ip[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
         syslog(LOG_INFO, "Accepted connection from %s", client_ip);
 
-        // Open/Create file
-        int fd = open(DATA_FILE, O_RDWR | O_CREAT | O_APPEND, 0666);
-        if (fd == -1) {
-            close(client_fd);
-            continue;
+        data_fd = open(DATA_FILE, O_RDWR | O_CREAT | O_APPEND, 0644);
+        
+        char *buffer = malloc(BUF_SIZE);
+        ssize_t bytes_received;
+        size_t total_received = 0;
+
+        // Receive until newline
+        while ((bytes_received = recv(client_fd, buffer + total_received, BUF_SIZE - 1, 0)) > 0) {
+            total_received += bytes_received;
+            if (memchr(buffer, '\n', total_received)) break;
+            buffer = realloc(buffer, total_received + BUF_SIZE);
         }
 
-        char buf[BUF_SIZE];
-        ssize_t bytes_recv;
-        
-        // Receive and write to file until newline
-        while ((bytes_recv = recv(client_fd, buf, BUF_SIZE, 0)) > 0) {
-            if (write(fd, buf, bytes_recv) == -1) break;
-            if (memchr(buf, '\n', bytes_recv)) break;
+        // Write to file
+        write(data_fd, buffer, total_received);
+
+        // THE FIX: Rewind file to beginning
+        lseek(data_fd, 0, SEEK_SET);
+
+        // Send whole file back
+        char send_buf[BUF_SIZE];
+        ssize_t r_bytes;
+        while ((r_bytes = read(data_fd, send_buf, BUF_SIZE)) > 0) {
+            send(client_fd, send_buf, r_bytes, 0);
         }
 
-        // Send file content back to client
-        lseek(fd, 0, SEEK_SET);
-        ssize_t bytes_read;
-        while ((bytes_read = read(fd, buf, BUF_SIZE)) > 0) {
-            send(client_fd, buf, bytes_read, 0);
-        }
-        
-        close(fd);
         close(client_fd);
+        close(data_fd);
+        free(buffer);
         syslog(LOG_INFO, "Closed connection from %s", client_ip);
     }
 
-    cleanup();
     return 0;
 }

415c07cabdbd3bf943dcbea757e7b3ea1af18b4b
ZatharV <atharvmore2622@gmail.com>
2026-02-15 04:06:06 -0700

    Assignment-5 final update

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,3 +1,12 @@
+/**
+ * 
+ * @file    aesdsocket.c
+ * @author  Atharv More
+ * @ref     https://gemini.google.com/share/3aa11fc7e916
+ * @date    02/14/2026
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,62 +28,110 @@ int server_fd = -1;
 int data_fd = -1;
 
 // Signal handler to ensure clean exit and file removal
-void handle_signal(int sig) {
-    if (sig == SIGINT || sig == SIGTERM) {
-        syslog(LOG_INFO, "Caught signal, exiting");
+void signal_handler(int sig) {
+    if (sig == SIGINT || sig == SIGTERM) 
+    {
+        syslog(LOG_INFO, "Sinal received: %s", (sig == SIGINT) ? "SIGINT" : ((sig == SIGTERM) ? "SIGTERM": ""));
         
-        if (server_fd != -1) close(server_fd);
-        // Delete the data file as required by the assignment
+        // If server_fd is created then close ir
+        if (server_fd != -1) 
+        {
+            close(server_fd);
+        }
         remove(DATA_FILE);
         closelog();
         exit(0);
     }
 }
 
-int main(int argc, char *argv[]) {
-    int daemon_mode = 0;
-    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
-        daemon_mode = 1;
-    }
+/**
+ * @brief   Checks if the program should be in daemon mode
+ * 
+ * @return  1 if daemon
+ *          0 otherwise
+ */
+int is_daemon_mode(int argc, char *argv[])
+{
+    if (argc > 1 && strcmp(argv[1], "-d") == 0)
+        return 1;
+    else
+        return 0;
+}
 
+/**
+ * @brief Sets signal Handing
+ */
+void set_signal_handling()
+{
+    // Setup signal handling
+    struct sigaction sig_action = {0};
+    sig_action.sa_handler = signal_handler;
+    sigaction(SIGINT, &sig_action, NULL);
+    sigaction(SIGTERM, &sig_action, NULL);
+}
+
+int main(int argc, char *argv[]) {
+    int daemon_mode = is_daemon_mode(argc, argv);
+
+    // Open Logs
     openlog("aesdsocket", LOG_PID, LOG_USER);
 
-    // Setup signal handling
-    struct sigaction sa;
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = handle_signal;
-    sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGTERM, &sa, NULL);
+    // Signal handling is important for SIGINT and SIGTERM
+    set_signal_handling();
 
     // Create and configure socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        syslog(LOG_ERR, "Socket creation failed: %m");
+    if (server_fd == -1) 
+    {
+        syslog(LOG_ERR, "Create Socket: Failed");
+        closelog();
         return -1;
     }
 
     int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+    {
+        syslog(LOG_ERR, "setsockopt: Failed");
+        close(server_fd);
+        closelog();
+        return -1;
+    }
 
-    struct sockaddr_in addr;
+    struct sockaddr_in addr = {0};
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(PORT);        // Convert port to Network Byte Order
+    addr.sin_addr.s_addr = INADDR_ANY;  // Bind to all available interfaces
 
     if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        syslog(LOG_ERR, "Bind failed: %m");
+        syslog(LOG_ERR, "Bind: failed");
         close(server_fd);
         return -1;
     }
 
-    // Daemonize if requested via -d argument
-    if (daemon_mode) {
+
+    // In daemon mode, it should run in the background
+    if (daemon_mode) 
+    {
         pid_t pid = fork();
-        if (pid < 0) return -1;
-        if (pid > 0) exit(0); // Parent exits
+
+        if (pid < 0)
+        {
+            syslog(LOG_ERR, "Forking failed in daemon mode");
+            closelog();
+            return -1;
+        }
+
+        if (pid > 0) 
+            exit(0); // In daemon mode, parent exits
         
-        if (setsid() < 0) return -1;
-        chdir("/");
+        if (setsid() < 0) 
+        {
+            return -1;
+        }
+
+        chdir("/"); // Change to root dir
+
+        // Redirect standard files to /dev/null
         int devnull = open("/dev/null", O_RDWR);
         dup2(devnull, STDIN_FILENO);
         dup2(devnull, STDOUT_FILENO);
@@ -82,55 +139,56 @@ int main(int argc, char *argv[]) {
         close(devnull);
     }
 
-    if (listen(server_fd, 10) < 0) {
-        syslog(LOG_ERR, "Listen failed: %m");
+    // Listen to connection
+    if (listen(server_fd, 10) < 0)
+    {
+        syslog(LOG_ERR, "Listen: Failed");
         close(server_fd);
         return -1;
     }
 
-    while (1) {
+    while(1) 
+    {
         struct sockaddr_in client_addr;
         socklen_t client_len = sizeof(client_addr);
         int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
         
-        if (client_fd == -1) continue;
+        if (client_fd == -1) 
+        {
+            continue;
+        }
 
         char client_ip[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
         syslog(LOG_INFO, "Accepted connection from %s", client_ip);
 
-        // Open/Create the data file for appending
         data_fd = open(DATA_FILE, O_RDWR | O_CREAT | O_APPEND, 0644);
         
         char *recv_buffer = malloc(BUF_SIZE);
         ssize_t bytes_received;
         size_t total_received = 0;
 
-        // Loop receiving until a newline (\n) is found
-        while ((bytes_received = recv(client_fd, recv_buffer + total_received, BUF_SIZE - 1, 0)) > 0) {
+        while ((bytes_received = recv(client_fd, recv_buffer + total_received, BUF_SIZE - 1, 0)) > 0) 
+        {
             total_received += bytes_received;
-            // Check for newline in the newly received chunk
             if (memchr(recv_buffer + (total_received - bytes_received), '\n', bytes_received)) {
                 break;
             }
             recv_buffer = realloc(recv_buffer, total_received + BUF_SIZE);
         }
 
-        // Write the full packet to the file
-        write(data_fd, recv_buffer, total_received);
 
-        // --- CRITICAL FIX: RESET FILE POINTER TO START ---
+        write(data_fd, recv_buffer, total_received);
         lseek(data_fd, 0, SEEK_SET);
         
 
-        // Read the entire cumulative file and send back to client
         char send_buf[BUF_SIZE];
         ssize_t r_bytes;
-        while ((r_bytes = read(data_fd, send_buf, BUF_SIZE)) > 0) {
+        while ((r_bytes = read(data_fd, send_buf, BUF_SIZE)) > 0) 
+        {
             send(client_fd, send_buf, r_bytes, 0);
         }
 
-        // Cleanup for this connection
         close(client_fd);
         close(data_fd);
         free(recv_buffer);
